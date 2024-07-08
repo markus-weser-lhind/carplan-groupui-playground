cf5843edf5ce9c89951114a9527974f1b2fb13c4
markus-weser-lhind <markus.weser@lhind.dlh.de>
2024-07-08 16:36:02 +0200

    feat: group-ui-angular-vwgroup-theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { GroupUiModule } from '@group-ui/group-ui-angular';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GroupUiModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'carplan-groupui-playground';
