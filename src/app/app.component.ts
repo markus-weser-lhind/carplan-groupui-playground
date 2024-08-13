b13d71c30d758627b7ca7dbe354f826eed726600
Markus Weser <markus.weser@lhind.dlh.de>
2024-08-13 10:41:33 +0200

    feat: import & usage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,25 @@ import {
   Weight,
 } from '@group-ui/group-ui-angular/node_modules/@group-ui/group-ui/dist/types';
 
+import {
+  CardModule,
+  FlatIconButtonModule,
+  HeaderControlsBarModule,
+  TagModule,
+  TagVariant,
+} from 'carplan-ui-components';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, GroupUiModule],
+  imports: [
+    RouterOutlet,
+    CardModule,
+    TagModule,
+    FlatIconButtonModule,
+    GroupUiModule,
+    HeaderControlsBarModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
@@ -20,4 +35,22 @@ export class AppComponent {
 
   theTypedTextSize: TextSize = 'caption';
   theTypedWeight: Weight = 'italic';
+
+  label = 'Button';
+
+  cardTitle = 'e-tron GT quattro';
+  content =
+    'Im Falle eines Problems wende dich bitte an das Audi Service Desk.';
+  imgSrc = 'assets/images/placeholder.png';
+  imgAlt = 'Placeholder image';
+  tag_1 = 'Gen 1';
+  tag_2 = 'PA';
+  count = '3';
+  variant = TagVariant.SIGNAL;
+
+  menuClick = (event: Event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    window.alert('Menu clicked ... 🎉');
+  };
 }
